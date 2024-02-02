96aa6c6afb28a07ef8b8d040fa9364dba6e9ca52
Sahil-Sham <sahilsham173@gmail.com>
2024-02-02 11:54:19 +0500

    use this line when you get string input

---
+++ class14.dart
@@ -0,0 +1,14 @@
+void main(){
+List<int> numbers=[2,3,4,5,6,93];
+for (var i in numbers) {
+  numberTable(i);
+  
+}
+}
+numberTable(int n){
+  for (var i = 1; i < 10; i++) {
+    print("$n x $i =${n * i}");
+    
+  }
+  print("=======================");
+}
---
+++ userinput.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main (){
+  var num1=stdin.readLineSync()!;
+  var num2 =stdin.readLineSync()!;
+print(num1+num2);
+}
