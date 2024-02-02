ffe93b620214911033de71aeed04aa29507f1c1b
Sahil-Sham <sahilsham173@gmail.com>
2024-02-02 12:46:35 +0500

    this is email or password code if email password failed so ask again

--- userinput.dart
+++ userinput.dart
@@ -1,7 +1,23 @@
 import 'dart:io';
 
+// void main (){
+//   var num1=int.parse(stdin.readLineSync()!);
+//   var num2 =int.parse(stdin.readLineSync()!);
+// print(num1+num2);
+// }
+
+
 void main (){
-  var num1=stdin.readLineSync()!;
-  var num2 =stdin.readLineSync()!;
-print(num1+num2);
+   bool islogin=false;
+  while(islogin==false){
+    var email=(stdin.readLineSync()!);
+  var password=stdin.readLineSync()!;
+    if (email=="sahilkumar@gmail.com" && password=="123456") {
+      print("login sucessfully");
+      islogin=true;
+      
+    }else{
+      print("login failed");
+    }
+  }
 }
